66af39a939fbca35d4234b1d8d3bf8712e44468c
HNatalia <kharchenko5365@gmail.com>
2021-04-09 20:11:12 +0300

    js for prices and carousel

--- dist/js/main.js
+++ dist/js/main.js
@@ -37,13 +37,52 @@
     }
   }
 
+  const carousel = () => {
+    const btnRight = document.querySelector('.btn-right');
+    const btnLeft = document.querySelector('.btn-left');
+    const pages = document.querySelectorAll('.review-page');
+    const items = document.querySelector('.items');
+    let index = 0;
+
+    const switchPage = index => {
+      items.style.transform = `translateX(-${100 * index}%)`;
+    }
+
+    btnRight.addEventListener('click', () => {
+      if (index > pages.length - 2) {
+        return;
+      }
+
+      switchPage(index + 1);
+      index++;
+    });
+
+    btnLeft.addEventListener('click', () => {
+      if (index === 0) {
+        return;
+      }
+
+      switchPage(index - 1);
+      index--;    
+    });
+  };
+
+  fetch ('https://api.unsplash.com/photos/?client_id=3g46TwjtuNPducXJTv7jO5BDjzNALqBZANYCMLQ4-Dc') 
+    .then(request => request.json())
+    .then(data => console.log(data))
+
+  // acsess key: 3g46TwjtuNPducXJTv7jO5BDjzNALqBZANYCMLQ4-Dc
+  // secret key: ZrAJBWAROl_E9BvTrcIBAoRU5A4YvK0q-QA4CgcF7oI
+
   document.addEventListener('DOMContentLoaded', () => {
     loaded();
     toggle();
+    carousel();
 
     window.addEventListener('scroll', () => {
       fadeIn('.grid');
-      fadeIn('.wrapper')
+      fadeIn('.wrapper');
+      fadeIn('.cards-wrapper');
     })
   })
 })();
