90ec3b7708265af456e6b18c1151d10307d9d4c5
HNatalia <kharchenko5365@gmail.com>
2021-04-15 15:58:34 +0300

    fetch api for portfolio&responsive&styles&modal window

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,9 +1,8 @@
 (() => {
-
   const loaded = () => {
     const body = document.querySelector('body');
     const loader = document.querySelector('.loader-container');
-    const page = document.querySelector('.page');
+    const page = document.querySelector('#page');
 
     setTimeout(() => {
       page.classList.add('fadeIn');
@@ -76,9 +75,114 @@
     });
   };
 
-  fetch ('https://api.unsplash.com/photos/?client_id=3g46TwjtuNPducXJTv7jO5BDjzNALqBZANYCMLQ4-Dc') 
+  const portfolio = () => {
+    const picture = document.querySelectorAll('#portfolio .picture');
+    const moreBtn = document.querySelector('#portfolio .more');
+    const addedGrid = document.querySelector('#portfolio .addedGrid');
+    const title = document.querySelectorAll('#portfolio h4');
+    const info = document.querySelectorAll('#portfolio a');
+    const projectModal = document.querySelector('#modal-project .modal');
+    const closeBtn = projectModal.querySelector('.close-btn');
+    const projectImg = projectModal.querySelector('img');
+    const projectTitle = projectModal.querySelector('h2');
+    
+    fetch ('https://api.unsplash.com/collections/1270212/photos/?client_id=3g46TwjtuNPducXJTv7jO5BDjzNALqBZANYCMLQ4-Dc') 
     .then(request => request.json())
-    .then(data => console.log(data))
+    .then(data => {
+      picture.forEach((el, index)=> {
+        el.style.backgroundImage = `url(${data[index].urls.small})`;
+      });
+
+      title.forEach((el, index) => {
+        el.textContent = `${data[index].user.name}`;
+      });
+
+      let titleArr = [];
+      let imgArr = [];
+      
+      for (let i = 0; i < data.length; i++) {
+        titleArr.push(data[i].user.name);
+        imgArr.push(data[i].urls.small);
+      }
+
+      info.forEach((el, index) => {
+        el.addEventListener('click', e => {
+          e.preventDefault();
+
+          projectModal.classList.add('open');
+
+          projectImg.src = imgArr[index];
+          projectTitle.textContent = titleArr[index];
+
+          closeBtn.addEventListener('click', () => {
+            projectModal.classList.remove('open');
+          });
+        });
+      });
+
+    });
+
+    moreBtn.addEventListener('click', () => {
+      moreBtn.style.display = 'none';
+
+      fetch ('https://api.unsplash.com/collections/1270212/photos/?page=2;client_id=3g46TwjtuNPducXJTv7jO5BDjzNALqBZANYCMLQ4-Dc') 
+      .then(request => request.json())
+      .then(data => {
+        console.log(data)
+        addedGrid.style.display = 'grid';
+
+        for (let i = 0; i < 10; i++) {
+          let divPictures = document.createElement('div');
+          divPictures.classList.add('addedPicture');
+
+          divPictures.innerHTML = `
+            <div class="hover">
+              <h4></h4>
+              <a href="">view more</a>
+            </div>
+          `;
+
+          addedGrid.appendChild(divPictures);
+        }
+
+        const addedPicture = document.querySelectorAll('#portfolio .addedPicture');
+        const addedTitle = addedGrid.querySelectorAll('.addedPicture h4');
+        const info = addedGrid.querySelectorAll('.addedPicture a');
+        
+        addedPicture.forEach((el, index) => {
+          el.style.backgroundImage = `url(${data[index].urls.small})`
+        });
+
+        addedTitle.forEach((el, index) => {
+          el.textContent = `${data[index].user.name}`;
+        });
+
+        let titleArr = [];
+        let imgArr = [];
+        
+        for (let i = 0; i < data.length; i++) {
+          titleArr.push(data[i].user.name);
+          imgArr.push(data[i].urls.small);
+        }
+
+        info.forEach((el, index) => {
+          el.addEventListener('click', e => {
+            e.preventDefault();
+
+            projectModal.classList.add('open');
+
+            projectImg.src = imgArr[index];
+            projectTitle.textContent = titleArr[index];
+
+            closeBtn.addEventListener('click', () => {
+              projectModal.classList.remove('open');
+            });
+          });
+        });
+      });
+    });
+  }
+
 
   // acsess key: 3g46TwjtuNPducXJTv7jO5BDjzNALqBZANYCMLQ4-Dc
   // secret key: ZrAJBWAROl_E9BvTrcIBAoRU5A4YvK0q-QA4CgcF7oI
@@ -86,6 +190,7 @@
   document.addEventListener('DOMContentLoaded', () => {
     loaded();
     toggle();
+    portfolio();
     carousel();
 
     window.addEventListener('scroll', () => {
